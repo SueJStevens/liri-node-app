71be60742adefab2f6faca9179b73b4f75d56365
SueJStevens <StevensStock@gmail.com>
2018-10-20 17:38:30 -0700

    Changes to code from testing + Markdown file

--- README.md
+++ README.md
@@ -1,2 +1,74 @@
-# liri-node-app
-Bootcamp Assignment 10/14/2018 - LIRI Bot
+# Timothy (aka liri-node-app)
+
+## What is Timothy?
+Timothy is an application that will search Spotify for songs, Bands in Town for concerts, and OMDB for movies.  It is sort-a-like SIRI but it can't take in voice commands.  The user must interface with Timothy through a text file or the command line.  Timothy will return useful information on par with the usefullness of the input provided by the user.  The user can input from the command line or through a text file.     
+
+Timothy started out as Homework Assignment #10 for UCSD Coding Bootcamp.  The official name of the assignment is 'Liri-Node_App'.  Since the assignment occured on the same week as C-Day, the application naturally morphed into Timothy.
+
+## Requirements
+- Make a command line Node.js app that depends on user input (parameters) from the command line and can also accept user input from text file.
+- The app will return formatted data from these APIs:
+  1. Bands in Town
+  2. Spotify
+  3. OMDB
+
+## Technology
+### Node.js
+- Node.js
+- Request (https://www.npmjs.com/package/request)
+- Moment (https://www.npmjs.com/package/moment)
+- DotEnv (https://www.npmjs.com/package/dotenv)
+- fs (npm install fs)
+### API (via npm module)
+- Bands In Town API (http://www.artists.bandsintown.com/bandsintown-api)
+- Spotify API (https://www.npmjs.com/package/node-spotify-api)
+- OMDB API (http://www.omdbapi.com)
+### Coding Language
+- JavaScript
+- Markdown
+
+## Code Explanation
+- Authentication keys for Spotify are stored in .env and accessed through the variables in keys.js.  This allows us to keep the API keys local (and confidential) while still providing other users of our app with the ability to obtain their own keys for Spotify authentication.  
+- Bands in Town API uses coding bootcamp's ID.
+- OMDB uses the key provided by Trilogy.
+- The application will return information from one of the three APIs depending on what the user requests via the command line or via the text file.  There are limits on the information returned and to be useful, the user must type in valid commands.
+  - concert-this + the name of an artist will return the concert venue hosting the requested artist that is located nearest to the user. (The logic behind this is simple:  SIRI always knows where you are.  All.The.Time.)
+  - spotify-this-song + the name of an album track will return up to 5 albums that contain the song and other useful information.
+  - movie-this + the name of a movie will return 
+  - do-what-it-says will return the same information as 'concert-this', 'spotify-this-song', or 'movie-this' except the value comes from the text file rather than from the user input on the command line.
+
+## Use Cases
+### Request a Concert
+#### User supplies valid arguments via command line for a concert that exists in Bands in Town API
+- Screen shot results here:
+#### User supplies valid arguments via command line for a concert that does not exist in Bands in Town API
+- Screen shot results here:
+#### User fails to supply value argument
+- Screen shot results here:
+#### User supplies arguments via text file.  Only the valid argument example shown here
+- Screen shot results here:
+
+
+### Request a Song
+#### User supplies valid arguments via command line for a song from an album track that exists in Spotify API
+- Screen shot results here:
+#### User supplies valid arguments via command line for a song from an album track that does not exist in Spotify API
+- Screen shot results here:
+#### User fails to supply value argument
+- Screen shot results here:
+#### User supplies arguments via text file.  Only the valid argument example shown here
+- Screen shot results here:
+
+### Request a Movie
+#### User supplies valid arguments via command line for a movie that exists in OMDB API
+- Screen shot results here:
+#### User supplies valid arguments via command line for a movie that does not exist in OMDB API
+- Screen shot results here:
+#### User fails to supply value argument
+- Screen shot results here:
+#### User supplies arguments via text file.  Only the valid argument example shown here
+- Screen shot results here:
+
+## Author & Copyright
+Sue J. Stevens (StevensStock@gmail.com) &copy;2018. All Rights Reserved.
+
--- liri.js
+++ liri.js
@@ -1,18 +1,75 @@
-function spotifyThisSong(valueIn) {
-  console.log("spotify-this-song");
-  console.log(valueIn);
+//add code to read and set any environment variables with the dotenv package:
+require("dotenv").config();
+
+//Add the code required to import the `keys.js` file and store it in a variable.
+var keys = require('./keys');
+
+//Access the spotify API
+var Spotify = require('node-spotify-api');
+
+var spotify = new Spotify(keys.spotify);
+// Store user supplied arguments in an array
+var nodeArgs = process.argv;
+
+//set up an array of valid user requests:
+var ArrValidOperators = [
+  "concert-this",
+  "spotify-this-song",
+  "movie-this",
+  "do-what-it-says",
+]
+
+//get user input & validate it
+if (process.argv[2] === undefined) {
+  //user did not provide an argument
+  console.log("You must make a request.")
+  console.log("Valid requests are:")
+  console.log(ArrValidOperators.join("\n"));
+} else {
+  //check to make sure the argument provided is valid
+  if (arrayContains(process.argv[2], ArrValidOperators) === false) {
+    console.log("Your request isn't valid.")
+    console.log("Valid requests are:")
+    console.log(ArrValidOperators.join("\n"));
+  } else {
+    //user supplied a valid operator so set the variable
+    var operator = process.argv[2];
+    //console.log("Operator: " + operator);
+
+    //set the value (it may not have been supplied)
+    if (process.argv[3] === undefined) {
+      var value = "";
+    } else {
+      //user might have supplied a multi-word value
+      var value = (concatValue(nodeArgs, 3, " "));
+    } //end test for value
+    //console.log("Value: " + value);
+    //call function processUserRequest
+    processUserRequest(operator, value);
+  } //end
+} //end test user input
+
+//process user request
+function processUserRequest(operator, value) {
+  identifyOperation(operator, value);
+}; //end process function
+
+//user requested a song
+function spotifyThisSong(operatorIn, valueIn) {
+  console.log("Operator: " + operatorIn);
+  console.log("Value: " + valueIn);
   console.log("=========================================");
 
-  //add code to read and set any environment variables with the dotenv package:
-  require("dotenv").config();
+  // //add code to read and set any environment variables with the dotenv package:
+  // require("dotenv").config();
 
-  //Add the code required to import the `keys.js` file and store it in a variable.
-  var keys = require('./keys');
+  // //Add the code required to import the `keys.js` file and store it in a variable.
+  // var keys = require('./keys');
 
-  //You should then be able to access the spotify API
-  var Spotify = require('node-spotify-api');
+  // //Access the spotify API
+  // var Spotify = require('node-spotify-api');
 
-  var spotify = new Spotify(keys.spotify);
+  // var spotify = new Spotify(keys.spotify);
 
   //set the name of the song to search for as a variable
   var trackName = valueIn;
@@ -41,21 +98,41 @@ function spotifyThisSong(valueIn) {
     .search({ type: 'track', query: trackName, limit: '5' })
     .then(function (data) {
 
-      console.log("Length: " + data.tracks.items.length)
+//      console.log(JSON.stringify(data));
+      //console.log(data.tracks.items.length);
+      //if(data.tracks.items.length != 0) {
+      //  console.log("I'm zero!!")
+     // };
 
       //test for situation where no song is returned
-      if (data.tracks.items.length != 0) {
+//      if (data.length.items!= 0 && data.tracks.items.length != 0) {
+  if (data.tracks.items.length != 0) {
 
         itemNum = 0;
         for (var i = 0; i < data.tracks.items.length; i++) {
-          console.log("Search for a song and return 5 items")
-          console.log(i + 1 + ".");
-          console.log("The Name of the Song is: " + data.tracks.items[i].name)
-          console.log("The Name of the Artist is: " + data.tracks.items[i].artists[0].name);
-          console.log("The Name of the Album is: " + data.tracks.items[i].album.name);
-          console.log("The Preview URL is: " + data.tracks.items[i].preview_url)
-          console.log("The ID: " + data.tracks.items[i].id)
-          console.log("-------------------------------------------------------------")
+          // showData ends up being the string containing the show data we will print to the console
+          var tracksData = [
+            i + 1 + ".",
+            "The Name of the Song is: " + data.tracks.items[i].name,
+            "The Name of the Artist is: " + data.tracks.items[i].artists[0].name,
+            "The Name of the Album is: " + data.tracks.items[i].album.name,
+            "The Preview URL is: " + data.tracks.items[i].preview_url,
+            "The ID: " + data.tracks.items[i].id,
+            "----------------------------------------------------------------------"
+          ].map(function (x) { return x.replace(/null/g, 'Not Available'); }).join("\n");
+          console.log(tracksData);
+
+
+          /*
+                    console.log("Search for a song and return 5 items")
+                    console.log(i + 1 + ".");
+                    console.log("The Name of the Song is: " + data.tracks.items[i].name)
+                    console.log("The Name of the Artist is: " + data.tracks.items[i].artists[0].name);
+                    console.log("The Name of the Album is: " + data.tracks.items[i].album.name);
+                    console.log("The Preview URL is: " + data.tracks.items[i].preview_url);
+                    console.log("The ID: " + data.tracks.items[i].id);
+                    console.log("-------------------------------------------------------------")
+          */
         }
 
       } else {
@@ -65,12 +142,15 @@ function spotifyThisSong(valueIn) {
           .request('https://api.spotify.com/v1/tracks/0hrBpAOgrt8RXigk83LLNE')
           .then(function (data) {
 
-            console.log("Request A Particular Song")
-            console.log("The Name of the Song is: " + data.name)
-            console.log("The Name of the Artist is: " + data.album.artists[0].name);
-            console.log("The Name of the Album is: " + data.album.name);
-            console.log("The Preview URL is: " + data.preview_url)
-            console.log("-------------------------------------------------------------")
+            var tracksData = [
+              "The Name of the Song is: " + data.album.name,
+              "The Name of the Artist is: " + data.album.artists[0].name,
+              "The Name of the Album is: " + data.name,
+              "The Preview URL is: " + data.preview_url,
+              "The ID: " + data.album.id,
+              "----------------------------------------------------------------------"
+            ].map(function (x) { return x.replace(/null/g, 'Not Available'); }).join("\n");
+            console.log(tracksData);
 
           })
           .catch(function (err) {
@@ -87,7 +167,11 @@ function spotifyThisSong(valueIn) {
 
 }; //end spotifyThisSong function
 
-function movieThis(valueIn) {
+//user requested a movie
+function movieThis(operatorIn, valueIn) {
+  console.log("Operator: " + operatorIn);
+  console.log("Value: " + valueIn);
+  console.log("=========================================");
 
   // Include the request npm package 
   var request = require("request");
@@ -100,43 +184,70 @@ function movieThis(valueIn) {
   var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
 
   // This line is just to help us debug against the actual URL.
-  console.log(queryUrl);
+  //console.log(queryUrl);
 
   request(queryUrl, function (error, response, body) {
 
     // If the request is successful
     if (!error && response.statusCode === 200) {
 
-      // Parse the body of the site to recover the required info
+      //test for situation where no song is returned
+      if (JSON.parse(body).Response != "False") {
 
-      // Title of the movie.
-      console.log("Title: " + JSON.parse(body).Title);
-      // Year the movie came out.
-      console.log("Release Year: " + JSON.parse(body).Year);
-      // IMDB Rating of the movie. 
-      console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
-      // Rotten Tomatoes Rating of the movie. //an array
-      for (var i = 0; i < JSON.parse(body).Ratings.length; i++) {
-        if (JSON.parse(body).Ratings[i].Source === 'Rotten Tomatoes') {
-          console.log("Rotten Tomatoes Ratings: " + JSON.parse(body).Ratings[i].Value);
-        }
+        // Parse the body of the site to recover the required info
+        console.log(movieData(body));
+
+      } else {
+        //return Mr. Nobody
+        console.log(JSON.parse(body).Error);
+        console.log("Maybe you would like this movie: \n");
+        var queryUrl = "http://www.omdbapi.com/?i=tt0485947&y=&plot=short&apikey=trilogy";
+        request(queryUrl, function (error, response, body) {
+          if (JSON.parse(body).Response != "False") {
+            // Parse the body of the site to recover the required info
+            console.log(movieData(body));
+          }
+        });
       }
-      // Country where the movie was produced.
-      console.log("Country: " + JSON.parse(body).Country);
-      // Language of the movie.
-      console.log("Language: " + JSON.parse(body).Language);
-      // Plot of the movie.
-      console.log("Plot: " + JSON.parse(body).Plot);
-      // Actors in the movie.
-      console.log("Actors: " + JSON.parse(body).Actors);
-
-
-    }
+    };
   });
+
+  function movieData(body) {
+
+    result = [
+      "Title: " + JSON.parse(body).Title,
+      "Release Year: " + JSON.parse(body).Year,
+      "IMDB Rating: " + JSON.parse(body).imdbRating,
+      "Rotten Tomatoes Ratings: " + ratingData(body),
+      "Country: " + JSON.parse(body).Country,
+      "Language: " + JSON.parse(body).Language,
+      "Plot: " + JSON.parse(body).Plot,
+      "Actors: " + JSON.parse(body).Actors,
+      "----------------------------------------------------------------------"
+    ].map(function (x) { return x.replace(/null/g, 'Not Available'); }).join("\n");
+
+    return result;
+
+  };
+
+  function ratingData(body) {
+    // Rotten Tomatoes Rating of the movie. //an array
+    for (var i = 0; i < JSON.parse(body).Ratings.length; i++) {
+      if (JSON.parse(body).Ratings[i].Source === 'Rotten Tomatoes') {
+        result = (JSON.parse(body).Ratings[i].Value);
+      }
+    };
+    return result;
+  };
 
 }; //end movie-this function
 
-function concertThis(valueIn) {
+//user requested a concert
+function concertThis(operatorIn, valueIn) {
+  console.log("Operator: " + operatorIn);
+  console.log("Value: " + valueIn);
+  console.log("=========================================");
+  var moment = require("moment");
 
   // Include the request npm package 
   var request = require("request");
@@ -146,34 +257,101 @@ function concertThis(valueIn) {
 
   // Then run a request to the OMDB API with the movie specified
   var queryUrl = "https://rest.bandsintown.com/artists/" + artistName + "/events?app_id=codingbootcamp";
-  console.log(queryUrl);
 
   // This line is just to help us debug against the actual URL.
-  console.log(queryUrl);
+  //console.log(queryUrl);
 
   request(queryUrl, function (error, response, body) {
 
     // If the request is successful
     if (!error && response.statusCode === 200) {
 
-      // Parse the body of the site to recover the required info
-      console.log("Venue: " + JSON.parse(body).length);
+      //test for empty return
+      if (JSON.parse(body).length === 0) {
+        console.log("We're sorry, but this band isn't playing near you.")
+      } else {
 
-      // Name of the venue
-      console.log("Venue: " + JSON.parse(body)[0].venue.name);
+        /*We want to display the venue that is the nearest location to where I am at now.
+         *In this example, I am at Lat 33.0990465 Long -117.267091
+         *In the real world, the "Siri" would know where I am.  --all-the-damn-time--like Santa Claus
+         *We are going to get from Bands In Town the Lat/Long of all the venues one by one
+         *Calc the distance between my location and the venue
+         *For the first venue populate the venue object 
+         *For all subsequent venues compare 
+         *If the comparison shows the venue closer than existing value in object then replace object value
+        */
 
-      // Venue location
-      console.log("Location: " + JSON.parse(body)[0].venue.city);
+        //Carlsbad CA Lat/Long
+        var myLat = 33.0990465;
+        var myLong = -117.267091;
 
-      // Date of the Event (use moment to format this as "MM/DD/YYYY")
-      console.log("Date: " + JSON.parse(body)[0].datetime);
+        //Seattle Washington Lat/Long
+        //var myLat = 47.6129432;
+        //var myLong = -122.4821475;
 
+        //Evans Georgia Lat/Long 
+        //var myLat = 33.5145179;
+        //var myLong = -82.2118206;
 
+        //Washington DC Lat/Long 
+        //var myLat = 38.890193;
+        //var myLong = -76.991921;
+
+        var venueLat = 0;
+        var venueLong = 0;
+        var distance = 0;
+        var distanceCompare = 0;
+        var venue = "";
+        var location = "";
+        var date = "";
+
+        // Parse the body of the site to recover the required info
+        var venueCount = JSON.parse(body).length;
+
+        //loop thru data
+        for (var i = 0; i < venueCount; i++) {
+          //Get The Lat/Long of Venue
+          venueLat = JSON.parse(body)[i].venue.latitude;
+          venueLong = JSON.parse(body)[i].venue.longitude;
+          //Calculate the Distance
+          distanceCompare = distanceInKmBetweenEarthCoordinates(myLat, myLong, venueLat, venueLong);
+          //        distanceCompare = Math.abs(myLat - venueLat) + Math.abs(myLong - venueLong);
+          //Compare the Distance
+          if (i === 0) {
+            distance = distanceInKmBetweenEarthCoordinates(myLat, myLong, venueLat, venueLong);
+            //          distance = Math.abs(myLat - venueLat) + Math.abs(myLong - venueLong);
+            //set Location, Venue and Date
+            venue = JSON.parse(body)[i].venue.name;
+            location = JSON.parse(body)[i].venue.city;
+            date = JSON.parse(body)[i].datetime;
+          } else {
+            //compare 
+            if (distanceCompare < distance) { //this venue is closer
+              //reset Location, Venue and Date
+              venue = JSON.parse(body)[i].venue.name;
+              location = JSON.parse(body)[i].venue.city;
+              date = JSON.parse(body)[i].datetime;
+            } //end compare
+          } //end compare loop
+        } //end of data loop
+
+        //Print Info
+
+        // Name of the venue
+        console.log("Venue: " + venue);
+
+        // Venue location
+        console.log("Location: " + location);
+
+        // Date of the Event (use moment to format this as "MM/DD/YYYY")
+        console.log("Date: " + moment(date).format("MM/DD/YYYY"));
+      }
     }
   });
 
 }; //end concertThis function
 
+//user's request is supplied in a text file called random.txt
 function doWhatItSays() {
 
   // fs is a core Node package for reading and writing files
@@ -189,37 +367,73 @@ function doWhatItSays() {
 
     // Then split it by commas and put in a data array
     var dataArr = data.split(",");
-    var operator = dataArr[0];
-    var value = dataArr[1];
-    console.log(operator);
-    console.log(value);
-
-    /** operator valid answers:
-       * `concert-this`
-       * `spotify-this-song`
-       * `movie-this`
-       * `do-what-it-says`
-    */
-
-    switch (operator) {
-      case "concert-this":
-        concertThis(value);
-        break;
-      case "spotify-this-song":
-        spotifyThisSong(value)
-        break;
-      case "movie-this":
-        movieThis(value)
-        break;
-      default:
-        result = "Unknown";
-    } //end switch
+    var operatorFromFile = dataArr[0];
+    var valueFromFile = dataArr[1];
+    processUserRequest(operatorFromFile, valueFromFile);
 
   }); //end fs.readFile
 
-
 } //end function doWhatItSays
 
-//call function doWhatItSays
-doWhatItSays();
+//function used to get distance between two points on a map
+function degreesToRadians(degrees) {
+  return degrees * Math.PI / 180;
+}
 
+//function used to get distance
+function distanceInKmBetweenEarthCoordinates(lat1, lon1, lat2, lon2) {
+  var earthRadiusKm = 6371;
+
+  var dLat = degreesToRadians(lat2 - lat1);
+  var dLon = degreesToRadians(lon2 - lon1);
+
+  lat1 = degreesToRadians(lat1);
+  lat2 = degreesToRadians(lat2);
+
+  var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  return earthRadiusKm * c;
+}
+
+//function to check if a string is in an array; returns true or false
+function arrayContains(needle, arrhaystack) {
+  return (arrhaystack.indexOf(needle) > -1);
+}
+
+//function to concatanate an array starting at any point and with any string
+function concatValue(arr, start, str) {
+  //create string of supplied values
+  var result = "";
+  for (var i = start; i < arr.length; i++) {
+    if (i > start && i < arr.length) {
+      result = result + str + arr[i];
+    }
+    else {
+      result += arr[i];
+    }
+  }
+  return result;
+} //end function concatValue
+
+//function to identify which operation the user requests
+function identifyOperation(operatorIn, valueIn) {
+  //console.log(operatorIn);
+
+  switch (operatorIn) {
+    case "concert-this":
+      concertThis(operatorIn, valueIn);
+      break;
+    case "spotify-this-song":
+      spotifyThisSong(operatorIn, valueIn);
+      break;
+    case "movie-this":
+      movieThis(operatorIn, valueIn);
+      break;
+    case "do-what-it-says":
+      doWhatItSays(valueIn);
+      break;
+    default:
+      result = "Unknown";
+  } //end switch  
+} //end function
